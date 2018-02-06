8e8cf29c670eb9577b9974091d49b0dc624549da
Alexandros Kozák <git-agkozak@sneakemail.com>
2018-02-06 08:20:49 -0800

    More "undefined index: sorp_styling"

--- inc/functions.php
+++ inc/functions.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * The actual SO Related Posts plugin files start here
- * For the function so_register_meta_boxes below I have taken the [demo.php file](https://github.com/rilwis/meta-box/blob/master/demo/demo.php) 
+ * For the function so_register_meta_boxes below I have taken the [demo.php file](https://github.com/rilwis/meta-box/blob/master/demo/demo.php)
  * of the Meta Box plugin and adapted it for the specific purpose of this SO Related Posts Plugin.
  *
  * @since 1.0
@@ -77,22 +77,22 @@ function so_related_posts_output( $content ) {
 	if ( is_main_query() && is_single() ) {
 
 		$so_show_related_posts = rwmb_meta( 'so_checkbox' );
-		$so_related_posts = rwmb_meta( 'so_related_posts', 'type=select_advanced' ); 
+		$so_related_posts = rwmb_meta( 'so_related_posts', 'type=select_advanced' );
 		$options = get_option( 'sorp_options' );
 		$sorp_title = $options['sorp_title'];
 		// check whether it has been set: http://wordpress.stackexchange.com/a/25409/2015
 		$sorp_showthumbs = isset( $options['sorp_showthumbs'] ) ? esc_attr( $options['sorp_showthumbs'] ) : '';
 
 		if ( $so_show_related_posts == 1 && ! empty( $so_related_posts ) ) {
-		
+
 			if ( ! empty( $sorp_title ) ) {
 				$content .= '<div class="so-related-posts"><h4>' . $sorp_title . '</h4><ul class="related-posts">';
 			} else {
 				$content .= '<div class="so-related-posts"><h4>' . __( 'Related Posts', 'so-related-posts' ) . '</h4><ul class="related-posts">';
 			}
-			
+
 			foreach ( $so_related_posts as $so_related_post ) {
-				
+
 				if ( $sorp_showthumbs == 1 && has_post_thumbnail( $so_related_post ) ) {
 					$feat_img = get_post_thumbnail_id( $so_related_post );
 					$img_url = wp_get_attachment_url( $feat_img,'full' ); //get full URL to image (use "large" or "medium" if the images too big)
@@ -103,12 +103,12 @@ function so_related_posts_output( $content ) {
 				} else {
 					$content .= '<li><a href="' . esc_url( get_permalink( $so_related_post ) ) . '" title="' . esc_attr( get_the_title( $so_related_post ) ) . '"><span class="title">' . esc_attr( get_the_title( $so_related_post ) ) . '</span></a></li>';
 				}
-			
+
 			};
-			
+
 			// @since 1.2.0
 			unset( $so_related_post );
-			
+
 			$content .= '</ul></div>';
 
 		}
@@ -121,19 +121,19 @@ function so_related_posts_output( $content ) {
 
 // @since 2.0.0
 function so_related_posts_styling() {
-	
+
 	$so_show_related_posts = rwmb_meta( 'so_checkbox' );
 
 	// is_main_query() to make sure that Related Posts Styling doesn't show elsewhere
 	if ( is_main_query() && is_single() && $so_show_related_posts == 1 ) {
 
 		$options = get_option( 'sorp_options' );
-		$sorp_styling = $options['sorp_styling'];
+		$sorp_styling = isset( $options['sorp_styling'] ) ? $options['sorp_styling'] : '';
 
 		if ( ! empty( $sorp_styling ) ) { ?>
-			
+
 			<style type="text/css" id="so-related-posts-css"><?php echo $sorp_styling; ?></style>
-			
+
 		<?php }
 	}
 }
